b476b676ff7958f77380c7429c92aeb3e6bd79cb
Christian Leicht <christianlj@ltaps.dk>
2019-05-29 20:37:44 +0200

    Added icons.scss

--- examples/app.ts
+++ examples/app.ts
@@ -1,11 +1,16 @@
 import {UIModal} from "../src/components/UIModal";
 import {DOM} from "../src/@core/utils/DOM";
+import "../src/@core/styles/@theme.scss";
 
 export class App {
 
     constructor() {
         DOM.query("#showModal").addEventListener("click", () => {
-            new UIModal({container: "body", title: "IFRAME with Wikipedia", iframe: "https://en.wikipedia.org/wiki/Ardbeg_distillery"});
+            new UIModal({
+                container: "body",
+                title: "IFRAME with Wikipedia",
+                iframe: "https://en.wikipedia.org/wiki/Ardbeg_distillery"
+            });
         });
 
         DOM.query("#showModalWithHtml").addEventListener("click", () => {
