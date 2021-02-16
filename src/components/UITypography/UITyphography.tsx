0612a1d3a8e7830fd51e9bb6d3a6b17205e026ec
Christian Leicht <christian@leicht.io>
2021-02-16 17:11:39 +0100

    feat: added bold to UITypography component.

--- src/components/UITypography/UITyphography.tsx
+++ src/components/UITypography/UITyphography.tsx
@@ -3,23 +3,28 @@ import {IProps} from './types';
 import './UITyphography.scss';
 
 export const UITypography = React.memo((props: IProps): ReactElement => {
+  let classes = '';
+  if (props.fontWeight) {
+    classes += `fw${props.fontWeight}`;
+  }
+
   switch (props.type) {
     case 'a':
-      return <a>{props.children}</a>;
+      return <a className={ classes }>{props.children}</a>;
     case 'h1':
-      return <h1>{props.children}</h1>;
+      return <h1 className={ classes }>{props.children}</h1>;
     case 'h2':
-      return <h2>{props.children}</h2>;
+      return <h2 className={ classes }>{props.children}</h2>;
     case 'h3':
-      return <h3>{props.children}</h3>;
+      return <h3 className={ classes }>{props.children}</h3>;
     case 'h4':
-      return <h4>{props.children}</h4>;
+      return <h4 className={ classes }>{props.children}</h4>;
     case 'h5':
-      return <h5>{props.children}</h5>;
+      return <h5 className={ classes }>{props.children}</h5>;
     case 'h6':
-      return <h6>{props.children}</h6>;
+      return <h6 className={ classes }>{props.children}</h6>;
     case 'p':
-      return <p>{props.children}</p>;
+      return <p className={ classes }>{props.children}</p>;
   }
 });
 
--- src/components/UITypography/types.ts
+++ src/components/UITypography/types.ts
@@ -1,4 +1,5 @@
 export interface IProps {
   type: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'a';
   children?: any;
+  fontWeight?: 300 | 700;
 }
